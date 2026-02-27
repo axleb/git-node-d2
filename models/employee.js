2c97870a32f15ab305b57fbaf45eef6799b99cfa
AxleB <axle.barr@skillsoft.com>
2026-02-27 12:56:02 -0400

    Updated employee model

--- models/employee.js
+++ models/employee.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect(
-  'mongodb://localhost:27017/Employees, 
+  'mongodb://localhost:27017/Employees', 
   { useUnifiedTopology: true },
   { useNewUrlParser: true}
 );
